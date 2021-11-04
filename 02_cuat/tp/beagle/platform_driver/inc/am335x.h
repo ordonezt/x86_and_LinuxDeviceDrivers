7b429a0d279d2ca0a9dd0283d4a7234e9dbf8486
toordonez <toordonez@frba.utn.edu.ar>
2021-11-04 20:48:59 +0000

    Listo el probe y remove, a por las fopsgit add .!

--- 02_cuat/tp/beagle/platform_driver/inc/am335x.h
+++ 02_cuat/tp/beagle/platform_driver/inc/am335x.h
@@ -1,28 +1,49 @@
-#define I2C2_REG                0x4819C000
-#define I2C2_REG_LEN            0x00001000
-#define I2C_IRQSTATUS           0x00000028
-#define I2C_IRQENABLE_SET       0x0000002C
-#define I2C_IRQENABLE_CLR       0x00000030
-#define RRDY_IE                 0x00000008
-#define XRDY_IE                 0x00000010
-#define I2C_DATA                0x0000009C
-#define I2C_CON                 0x000000A4
-#define I2C_CON_START           0x00000001
-#define I2C_CON_STOP            0x00000002
-#define I2C_PSC                 0x000000B0
-#define I2C_SCLL                0x000000B4
-#define I2C_SCLH                0x000000B8    
-#define I2C_OA                  0x000000A8
-#define I2C_SA                  0x000000AC
-#define I2C_IRQSTATUS_RAW       0x00000024
-#define I2C_CNT                 0x00000098
+#ifndef _AM335X_H
+#define _AM335X_H
 
-#define CM_PER_REG              0x44E00000
-#define CM_PER_REG_LEN          0x00000400
-#define CM_PER_I2C2_CLKCTRL     0x00000044
+#define CM_PER_REG              0x44E00000                  // Direccion base del bloque de registros del clock module Section 8.1.12
+#define CM_PER_I2C2_CLKCTRL     0x00000044                  //Section 8.1.12.1.15
+#define CM_PER_REG_LEN          CM_PER_I2C2_CLKCTRL + 4     // Longitud del bloque de registros
 
 #define CTRL_MODULE_REG         0x44E10000
 #define CTRL_MODULE_REG_LEN     0x00002000
 #define CONF_UART1_CSTN         0x00000978
 #define CONF_UART1_RSTN         0x0000097C
 #define PIN_I2C_CFG             0x00000023
+
+
+#define I2C2_REG                0x4819C000      // Direccion base fisica del comienzo del bloque de registros de I2C2 Section 21.4.1
+#define I2C_REVNB_LO            0x00            // Module Revision Register (low bytes) Section 21.4.1.1
+#define I2C_REVNB_HI            0x04            // Module Revision Register (high bytes) Section 21.4.1.2
+#define I2C_SYSC                0x10            // System Configuration Register Section 21.4.1.3
+#define I2C_IRQSTATUS_RAW       0x24            // I2C Status Raw Register Section 21.4.1.4
+#define I2C_IRQSTATUS           0x28            // I2C Status Register Section 21.4.1.5
+#define I2C_IRQENABLE_SET       0x2C            // I2C Interrupt Enable Set Register Section 21.4.1.6
+#define I2C_IRQENABLE_CLR       0x30            // I2C Interrupt Enable Clear Register Section 21.4.1.7
+#define I2C_WE I2C              0x34            // Wakeup Enable Register Section 21.4.1.8
+#define I2C_DMARXENABLE_SET     0x38            // Receive DMA Enable Set Register Section 21.4.1.9
+#define I2C_DMATXENABLE_SET     0x3C            // Transmit DMA Enable Set Register Section 21.4.1.10
+#define I2C_DMARXENABLE_CLR     0x40            // Receive DMA Enable Clear Register Section 21.4.1.11
+#define I2C_DMATXENABLE_CLR     0x44            // Transmit DMA Enable Clear Register Section 21.4.1.12
+#define I2C_DMARXWAKE_EN        0x48            // Receive DMA Wakeup Register Section 21.4.1.13
+#define I2C_DMATXWAKE_EN        0x4C            // Transmit DMA Wakeup Register Section 21.4.1.14
+#define I2C_SYSS                0x90            // System Status Register Section 21.4.1.15
+#define I2C_BUF                 0x94            // Buffer Configuration Register Section 21.4.1.16
+#define I2C_CNT                 0x98            // Data Counter Register Section 21.4.1.17
+#define I2C_DATA                0x9C            // Data Access Register Section 21.4.1.18
+#define I2C_CON                 0xA4            // I2C Configuration Register Section 21.4.1.19
+#define I2C_OA                  0xA8            // I2C Own Address Register Section 21.4.1.20
+#define I2C_SA                  0xAC            // I2C Slave Address Register Section 21.4.1.21
+#define I2C_PSC                 0xB0            // I2C Clock Prescaler Register Section 21.4.1.22
+#define I2C_SCLL                0xB4            // I2C SCL Low Time Register Section 21.4.1.23
+#define I2C_SCLH                0xB8            // I2C SCL High Time Register Section 21.4.1.24
+#define I2C_SYSTEST             0xBC            // System Test Register Section 21.4.1.25
+#define I2C_BUFSTAT             0xC0            // I2C Buffer Status Register Section 21.4.1.26
+#define I2C_OA1 I2C             0xC4            // Own Address 1 Register Section 21.4.1.27
+#define I2C_OA2 I2C             0xC8            // Own Address 2 Register Section 21.4.1.28
+#define I2C_OA3 I2C             0xCC            // Own Address 3 Register Section 21.4.1.29
+#define I2C_ACTOA               0xD0            // Active Own Address Register Section 21.4.1.30
+#define I2C_SBLOCK              0xD4            // I2C Clock Blocking Enable Register Section 21.4.1.31
+#define I2C_REG_LEN             I2C_SBLOCK + 4  // Longitud del bloque de registros
+
+#endif /*_AM335X_H*/
--- 02_cuat/tp/beagle/platform_driver/src/TBO_td3_i2c_dev.c
+++ 02_cuat/tp/beagle/platform_driver/src/TBO_td3_i2c_dev.c
@@ -19,25 +19,21 @@
 #include <linux/wait.h>
 
 #include "../inc/TBO_td3_i2c_dev.h"
+#include "../inc/am335x.h"
 #include "../inc/mpu6050.h"
 
 /* PROTOTIPOS */
 static int __init i2c_init(void);
 static void __exit i2c_exit(void);
 static int cambiar_permisos_cdev(struct device *dev, struct kobj_uevent_env *env);
+irqreturn_t I2C_IRQ_Handler(int IRQ, void *ID, struct pt_regs *REG);
 
 static int i2c_probe(struct platform_device * i2c_pd);
 static int i2c_remove(struct platform_device * i2c_pd);
 
-MODULE_LICENSE("Dual BSD/GPL");
-MODULE_AUTHOR("Tomás Bautista Ordóñez");
-MODULE_VERSION("0.1");
-MODULE_DESCRIPTION("Driver I2C");
-
-// MODULE_DEVICE_TABLE(of, i2c_of_device_ids);
-
-module_init(i2c_init);
-module_exit(i2c_exit);
+/* Variables globales*/
+static void __iomem *I2C2_Base, *CM_PER_Base, *CTRL_MODULE_Base;    //Mapeo de registros
+int virq;                                                           //Interrupcion virtual
 
 struct driver{
     dev_t type;             //Tipo de dispositivo (Numero mayor y menor)
@@ -72,6 +68,17 @@ static struct platform_driver i2c_platform_driver = { //Handlers del platform dr
         .of_match_table = of_match_ptr(i2c_of_device_ids)
     },
 };
+
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Tomás Bautista Ordóñez");
+MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("Driver I2C");
+
+// MODULE_DEVICE_TABLE(of, i2c_of_device_ids);
+
+module_init(i2c_init);
+module_exit(i2c_exit);
 
 /**
  * @brief Instala el modulo driver I2C en el sistema
@@ -187,14 +194,119 @@ static int cambiar_permisos_cdev(struct device *dev, struct kobj_uevent_env *env
 }
 
 static int i2c_probe(struct platform_device * i2c_pd){
+    uint32_t reg_value_aux;
+
     pr_info("Driver: Entre al probe\n");
 
-    //Mapeo de registros
+    /**
+     * La configuracion consta de los siguientes pasos:
+     *  -> Configuracion de los registros necesarios para manejar el I2C en el am335x
+     *  -> Configurar el clock de I2C
+     *  -> Configurar los pines
+     *  -> Configurar el prescaler
+     *  -> Sacar el periferico de reset
+     *  -> Configurar el modo
+     *  -> Habilitar interrupciones
+     *  -> Configurar direccion de esclavo
+     *  -> Configurar los contadores de datos
+     * 
+     * Para hacer eso primero es necesario mapear los registros que haya que tocar, habilitar el clock, pedir la/s interupcion/es virtual/es y configurar los pines
+     * */
+
+    //Mapeamos el bloque de registros de I2C2
+    I2C2_Base = ioremap(I2C2_REG, I2C_REG_LEN);
+    if(I2C2_Base == NULL){
+        pr_err("Driver: No se pudo mapear el I2C2\n");
+        return -1;
+    }
+    pr_info("Driver: I2C2 mapeado %X\n", (uint32_t)I2C2_Base);
+    
+    //Mapeamos CM_PER
+    CM_PER_Base = ioremap(CM_PER_REG, CM_PER_REG_LEN);
+    if(CM_PER_Base == NULL){
+        pr_err("Driver: No se pudo mapear CM_PER\n");
+        iounmap(I2C2_Base);
+        return -1;
+    }
+    pr_info("Driver: CM_PER mapeado %X\n", (uint32_t)CM_PER_Base);
+
+    //Mapeamos CTRL_MODULE
+    CTRL_MODULE_Base = ioremap(CTRL_MODULE_REG, CTRL_MODULE_REG_LEN);  
+    if(CTRL_MODULE_Base == NULL){
+        pr_err("Driver: No se pudo mapear CTRL_MODULE\n");
+        iounmap(I2C2_Base);    
+        iounmap(CM_PER_Base);
+        return -1;    
+    }
+    pr_info("Driver: CTRL_MODULE mapeado %X\n", (uint32_t)CTRL_MODULE_Base);
+
+    //Habilitamos el clock del I2C2 seccion 8.1.12.1.15
+    reg_value_aux = ioread32(CM_PER_Base + CM_PER_I2C2_CLKCTRL);
+    reg_value_aux |= 0x02;
+    iowrite32(reg_value_aux,CM_PER_Base + CM_PER_I2C2_CLKCTRL); //MODULEMODE = ENABLE
+    msleep(100);
+    reg_value_aux = ioread32(CM_PER_Base + CM_PER_I2C2_CLKCTRL);
+    if((reg_value_aux & 0x03) != 0x02){
+        pr_err("Driver: No se pudo habilitar el clock del I2C2\n");
+        iounmap(I2C2_Base);    
+        iounmap(CM_PER_Base);    
+        iounmap(CTRL_MODULE_Base);    
+        return -1;
+    }
+
+    //Configuramos los pines como SDA y SCL seccion 9.2.1 y Pagina 1515
+    //PIN 20 : UART1_Ctsn - I2C2_SDA
+    reg_value_aux = ioread32(CTRL_MODULE_Base + CONF_UART1_CSTN);
+    //Reserved | Fast | Receiver Enable | PullDown Selected | PullUp/Pulldown Enable | I2C2_SCL
+    //    0       0             1               0                         0            011
+    //0x23
+    reg_value_aux = PIN_I2C_CFG;
+    iowrite32(reg_value_aux, CTRL_MODULE_Base + CONF_UART1_CSTN);
+
+    //PIN 19 : UART1_rtsn - I2C2_SCL
+    reg_value_aux = ioread32(CTRL_MODULE_Base + CONF_UART1_RSTN);
+    reg_value_aux = PIN_I2C_CFG;
+    iowrite32(reg_value_aux,CTRL_MODULE_Base + CONF_UART1_RSTN);
+    pr_info("Driver: Clock I2C2 y pines SDA y SCL configurados\n");    
+
+    //Interrupciones virtuales
+    virq = platform_get_irq(i2c_pd,0);
+    if(virq < 0){
+        pr_err("Driver: No pudo obtener la virtual interrupt request\n");
+        iounmap(I2C2_Base);    
+        iounmap(CM_PER_Base);    
+        iounmap(CTRL_MODULE_Base);    
+        return -1;
+    }
+    
+    if(request_irq(virq, (irq_handler_t) I2C_IRQ_Handler, IRQF_TRIGGER_RISING, COMPATIBLE, NULL)){
+        pr_err("Driver: No se pudo asignar la virtual interrupt request\n");
+        iounmap(I2C2_Base);    
+        iounmap(CM_PER_Base);    
+        iounmap(CTRL_MODULE_Base);    
+        return -1;
+    }  
+    pr_info("Driver: Interrupcion virtual configurada\n");
+
+    pr_info("Driver: Saliendo del probe\n");
 
     return 0;
 }
 
 static int i2c_remove(struct platform_device * i2c_pd){
+    pr_info("Driver: Removiendo modulo\n");
+
+    free_irq(virq,NULL);
+    pr_info("Driver: Interrupciones virtuales liberadas\n");
+
+    iounmap(I2C2_Base);    
+    pr_info("Driver: Registros I2C2 liberados\n");
+
+    iounmap(CM_PER_Base);    
+    pr_info("Driver: Registros CM_PER liberados\n");
+
+    iounmap(CTRL_MODULE_Base);
+    pr_info("Driver: Registros CTRL_MODULE liberados\n");
 
     pr_info("Driver: Removi el platform device\n");
     
@@ -224,4 +336,8 @@ ssize_t sensor_write (struct file *flip, const char __user *buf, size_t count, l
 long sensor_ctrl(struct file *flip, unsigned int cmd, unsigned long values){
     printk(KERN_INFO "Pase por ioctl\n");
     return 0;
+}
+
+irqreturn_t I2C_IRQ_Handler(int IRQ, void *ID, struct pt_regs *REG){
+    return IRQ_HANDLED;
 }
